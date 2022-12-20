89ac92c413db2e2b68750db29ab3af234b599d10
paul rublev <paulrublev@Mac.local>
2022-12-20 23:29:59 +0500

    Add bottom panel

--- lib/src/widgets/categories_view.dart
+++ lib/src/widgets/categories_view.dart
@@ -8,6 +8,8 @@ class CategoriesView extends StatefulWidget {
 }
 
 class _CategoriesViewState extends State<CategoriesView> {
+  final _editingController = TextEditingController(text: '');
+
   @override
   void initState() {
     super.initState();
@@ -17,16 +19,43 @@ class _CategoriesViewState extends State<CategoriesView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Categories'),
+        title: const Text('Images'),
       ),
-      body: ListView.builder(
-        itemCount: [].length,
-        itemBuilder: (context, index) {
-          return SizedBox(
-            height: 50,
-            child: Container(),
-          );
-        },
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: [].length,
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  height: 50,
+                  child: Container(),
+                );
+              },
+            ),
+          ),
+          Container(
+            color: Colors.amber,
+            height: 70,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _editingController,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Save'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
