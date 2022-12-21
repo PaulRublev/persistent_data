ce9466274d8b7cd8f52ab0dd5f2fd7050c093b13
paul rublev <paulrublev@Mac.local>
2022-12-21 16:04:34 +0500

    Initial commit

--- lib/src/widgets/app.dart
+++ lib/src/widgets/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'images_view.dart';
+import 'home_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -12,7 +12,7 @@ class App extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ImagesView(),
+      home: const HomePage(),
     );
   }
 }
